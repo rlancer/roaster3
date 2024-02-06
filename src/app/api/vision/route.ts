5f8208d2918f6847b9fae8c33b252cd509819f79
Robert Lancer <rlancer@users.noreply.github.com>
2024-02-06 22:56:09 +0000

    roasting

--- src/app/api/vision/route.ts
+++ src/app/api/vision/route.ts
@@ -7,24 +7,86 @@ const openai = new OpenAI({
 });
 
 export async function POST(req: NextRequest) {
-  const { base64, prompt } = await req.json()
+  const prompt = `
+  Return a short visual description in the key 'short_visual_description', 
+  and a more detailed description in the key 'short_visual_description' 
+  and their position in the key 'position' as laid out in the following example JSON response:
+
+  {
+    "people" : 
+    [
+      {"short_visual_description": "Wears glasses", 
+      "long_visual_description": "The persons shirt is pink. Has light skin color.", 
+      "position":"Upper right"},
+      {"short_visual_description": "Balck hair", "long_visual_description": "The persons shirt is pink. Has light skin color.", "position":"Upper right"}
+    ]
+  }
+`
+
+  const { base64 } = await req.json()
   try {
-    const completions = await openai.chat.completions.create({
-      max_tokens: 1024,
+    const completions = await openai.chat.completions.create({      
+      max_tokens:2000,
       messages: [
-        { role: "user",content: [
-        {"type": "text", "text": prompt },
-        {
-          "type": "image_url",
-          "image_url": {
-            "url": `${base64}`,
-          },
-        },
-        ]
-    }
+         {
+          role: "user", content: [
+            { "type": "text", "text": prompt },
+            {
+              "type": "image_url",
+              "image_url": {
+                "url": `${base64}`,
+              },
+            },
+          ]
+        }
       ],
-      model: "gpt-4-vision-preview",
+      model:"gpt-4-vision-preview",
+
     });
+
+    const jsonTxt=  completions.choices[0].message.content;
+
+    console.log(jsonTxt);
+
+    const srippedJson = jsonTxt?.replace('```json', '').replace('```','');
+
+
+    const completionsRoast = await openai.chat.completions.create({
+      response_format:{type:'json_object'},
+      model: 'gpt-4-1106-preview',
+      messages: [
+        {
+          role: "system",
+          content: "You are a commedian in the style of an insult comic."
+        },
+        {
+          role: 'assistant',
+          content: `For each Person in the array of 'people', take the 'long_visual_description' field and create a light hearted insult.
+
+          The response should be in following JSON format:
+
+          {
+            "people":
+            [
+              {
+                "short_visual_description": "same value from the input",
+                "joke": "the joke based on the 'long_visual_description' from the input",
+                "position": "the same value from the input"
+              }
+            ]
+          }
+
+          Input json:
+
+          ${srippedJson}
+          `
+        }
+      ],
+    });
+
+    console.log('jokes', completionsRoast.choices[0].message);
+
+
     return NextResponse.json(completions);
   } catch (error) {
     console.error('OpenAI API error:', error);
